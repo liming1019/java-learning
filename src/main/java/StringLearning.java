9411e4ab8af74cd251483936cc91b6fb3c8153d5
liming831019 <liming831019@hotmail.com>
2017-08-20 00:26:16 +0800

    gitignore

--- src/main/java/StringLearning.java
+++ src/main/java/StringLearning.java
@@ -1,8 +1,16 @@
 public class StringLearning {
     public static void main(String[] args){
-        StringBuffer sb = new StringBuffer();
+       /* StringBuffer sb = new StringBuffer();
         System.out.print(sb.length());
         sb.append("hello");
         System.out.print(sb.length());
+*/
+
+
+        StringBuffer sb2 = new StringBuffer("hello world!");
+        System.out.print(sb2.length());
+
+        StringBuilder sb =new StringBuilder();
+        sb.append("a");
     }
 }
