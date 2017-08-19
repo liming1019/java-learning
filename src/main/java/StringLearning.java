e0d248ad7615c4e1ae430a32d679c403abaee599
liming831019 <liming831019@hotmail.com>
2017-08-20 00:00:47 +0800

    Initial commit

---
+++ src/main/java/StringLearning.java
@@ -0,0 +1,8 @@
+public class StringLearning {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer();
+        System.out.print(sb.length());
+        sb.append("hello");
+        System.out.print(sb.length());
+    }
+}
